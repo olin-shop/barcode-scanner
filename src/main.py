25511c8f5a49f4d4c6c65c00644878e847cc6011
rami <ramzey.burdette2005@gmail.com>
2026-06-18 11:24:38 -0400

    updated main to switch to quart

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 Main file.
 """
 
-from backend.endpoints import flask_app
+from backend.endpoints import quart_app
 from backend.backend_constants import HOST_IP, PORT
 
 
@@ -10,7 +10,8 @@ def main() -> None:
     """
     Main method.
     """
-    flask_app.run(host=HOST_IP, port=PORT)
+    # Quart automatically manages the asyncio event loop for you
+    quart_app.run(host=HOST_IP, port=PORT)
 
 
 if __name__ == "__main__":
